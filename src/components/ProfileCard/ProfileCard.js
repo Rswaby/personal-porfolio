ac99ef90f7858ba068064e0c755e7dcce391407e
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-01-11 14:13:17 -0500

    small changes

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -6,7 +6,7 @@ const ProfileCard = () => (
         <Image src='https://res.cloudinary.com/roro-personal/image/upload/v1546480599/IMG_0258.jpg'/>
         <Card.Content>
             <Card.Header>Rohan Swaby</Card.Header>
-            <Card.Description><p>Software Developer at The city College Of New York</p></Card.Description>
+            <Card.Description><p>Software Engineer at The city College Of New York</p></Card.Description>
         </Card.Content>
         <Card.Content extra>
             <Icon name='graduation' />
