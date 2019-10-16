e9d2c745eec90b035f967aafe20e22eb308f7e65
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-10-16 17:39:12 -0400

    added main wrapper

---
+++ src/layouts/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Navbar from '../components';
+
+
+class Main extends Component {
+    componentWillMount(){
+        window.scrollTo(0,0);
+    }
+
+    render(){
+        return (
+            <div id="main-wrapper">
+
+            </div>
+        )
+    }
+}
